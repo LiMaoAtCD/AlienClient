c2b851dea575778cc74f11b3a28db6ce12ff097e
liamscofield <liamscofield@gmail.com>
2014-12-17 09:31:52 +0800

    ALNNetworkHandler added

---
+++ AlienClient/ALNNetworkHandler.swift
@@ -0,0 +1,23 @@
+//
+//  ALNNetworkHandler.swift
+//  AlienClient
+//
+//  Created by AlienLi on 14/12/16.
+//  Copyright (c) 2014年 ALN. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+class ALNNetworkHandler: NSObject {
+    
+    class func request(string: String, params: [String:AnyObject]?, WithCompletion completionHandler:((NSURLRequest, NSHTTPURLResponse?, AnyObject?, NSError?) -> Void)){
+        if string == "GET" {
+            Alamofire.request(.GET, "http://192.168.3.107:3000", parameters: params, encoding: .URL).responseJSON(completionHandler:completionHandler)
+        } else if string == "POST" {
+            Alamofire.request(.POST,  "http://192.168.2.132:3000", parameters: params, encoding: .URL).responseJSON(completionHandler:completionHandler)
+        } else {
+            println("HTTP Method Error")
+        }
+    }
+   
+}
--- AlienClientTests/AsyncTests.swift
+++ AlienClientTests/AsyncTests.swift
@@ -11,11 +11,54 @@ import XCTest
 import Quick
 import Nimble
 
+
 class AsyncTests: QuickSpec {
 
     override func spec() {
+        var handler: ALNNetworkHandler?
+        
         beforeEach { () -> () in
-            
+            handler = ALNNetworkHandler()
         }
+        
+        afterEach { () -> () in
+            handler = nil
+        }
+        
+        describe("there is a controller of ", { () -> () in
+            it("should be not nil", { () -> () in
+                expect({ () -> ALNNetworkHandler? in
+                    return handler
+                }).notTo(beNil())
+            })
+        })
+        
+        describe("ALNNetworkHandler has a get method", { () -> () in
+
+            it("should be get", { () -> () in
+                var callback: AnyObject?
+//                handler!.request(.GET,["account":"alien","pwd":"123456"],.URL).responseJSON{
+//                (request,response,JSON,error) in
+//                    callback = JSON
+//                }
+             
+//                expect({ () -> Any? in
+//                    return callback
+//                }).toEventually(beTruthy(), timeout:3)
+//                handler!.request("GET", params: ["account": "alien", "pwd": "123456"]).responseJSON{
+//                    (request,response,JSON,error) in
+//                    callback = JSON
+//                }
+                
+                ALNNetworkHandler.request("GET", params: ["username":"alien","pwd":"123456","act":"query"], WithCompletion: { (request, response, JSON, error) -> Void in
+                    println(JSON)
+                    callback = JSON
+                })
+                
+                expect(callback).toEventuallyNot(beNil(), timeout: 3.0, pollInterval: 0.5)
+
+            })
+
+        })
     }
 }
